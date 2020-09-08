2f8d597a8f45e0b8dd22911fc4db755b93d8179f
alex <alexmolina@gmail.com>
2020-09-09 01:27:42 +0200

    Añadida rueda y boton foward

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
-      title: 'iPod',
-      home: Scaffold(
-        body:MyIpod()
-      )
-    );
+        theme: ThemeData(brightness: Brightness.dark),
+        title: 'iPod',
+        home: Scaffold(body: MyIpod()));
   }
 }
-
 
 class MyIpod extends StatefulWidget {
   @override
@@ -24,7 +18,6 @@ class MyIpod extends StatefulWidget {
 }
 
 class _MyIpodState extends State<MyIpod> {
-
   final PageController _pageCtrl = PageController(viewportFraction: 0.6);
   double currentPage = 0.0;
 
@@ -37,28 +30,72 @@ class _MyIpodState extends State<MyIpod> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
         children: <Widget>[
           Container(
-            height: 300.0,
-            color: Colors.black,
-            child: PageView.builder(
-              controller: _pageCtrl,
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context , int currentIndx){
-                return AlbumCard(
-                  color: Colors.accents[currentIndx],
-                  currentIndx: currentIndx,
-                  currentPage : currentPage
-                );
-              },
-            )
-          )
+              height: 300.0,
+              color: Colors.black,
+              child: PageView.builder(
+                controller: _pageCtrl,
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, int currentIndx) {
+                  return AlbumCard(
+                      color: Colors.accents[currentIndx],
+                      currentIndx: currentIndx,
+                      currentPage: currentPage);
+                },
+              )),
+          Spacer(),
+          Center(
+              child: Stack(
+            alignment: Alignment.center,
+            children: [
+              GestureDetector(
+                onPanUpdate: (e) => null,
+                child: Container(
+                  height: 300.0,
+                  width: 300.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        child: Text('MENU',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold)),
+                        alignment: Alignment.topCenter,
+                        margin: EdgeInsets.only(top: 36),
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(Icons.fast_forward),
+                          iconSize: 40.0,
+                          onPressed: () => _pageCtrl.animateToPage(
+                              (_pageCtrl.page + 1).toInt(),
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeIn),
+                        ),
+                        alignment: Alignment.centerRight,
+                        margin: EdgeInsets.only(right: 30),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white38,
+                ),
+              )
+            ],
+          )),
         ],
       ),
     );
@@ -66,12 +103,11 @@ class _MyIpodState extends State<MyIpod> {
 }
 
 class AlbumCard extends StatelessWidget {
-
   final Color color;
   final int currentIndx;
   final double currentPage;
 
-  AlbumCard({this.color,this.currentPage,this.currentIndx});
+  AlbumCard({this.color, this.currentPage, this.currentIndx});
 
   @override
   Widget build(BuildContext context) {
@@ -80,29 +116,23 @@ class AlbumCard extends StatelessWidget {
     return Container(
       width: 250,
       child: Transform(
-        transform: Matrix4.identity()
+          transform: Matrix4.identity()
             ..setEntry(3, 2, 0.003) //añadir perspectiva
-            ..scale((1-relativePosition.abs()).clamp(0.2, 0.6)+0.4)
+            ..scale((1 - relativePosition.abs()).clamp(0.2, 0.6) + 0.4)
             ..rotateY(relativePosition),
-
-        alignment:  relativePosition >= 0
-          ? Alignment.centerLeft
-          : Alignment.centerRight,
-
-        child: Container(
-          margin: EdgeInsets.fromLTRB(5,20,5,20),
-          padding: EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: color,
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('images/album$currentIndx.jpg')
-            )
-          ),
-        )
-
-      ),
+          alignment: relativePosition >= 0
+              ? Alignment.centerLeft
+              : Alignment.centerRight,
+          child: Container(
+            margin: EdgeInsets.fromLTRB(5, 20, 5, 20),
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: color,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('images/album$currentIndx.jpg'))),
+          )),
     );
   }
 }
